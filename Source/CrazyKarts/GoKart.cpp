6643697cc8d718b6793267dc5bfe8be47d4fcd8d
HyunwookYoo <hyunwook5636@gmail.com>
2021-05-19 01:35:14 +0900

    Start Making CrazyKarts

---
+++ Source/CrazyKarts/GoKart.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	FVector Acceleration = Force / Mass;
+
+	Velocity += Acceleration * DeltaTime;
+
+	UpdateCarRotation(DeltaTime);
+
+	UpdateLocationFromVelocity(DeltaTime);
+}
+
+void AGoKart::UpdateCarRotation(float DeltaTime)
+{
+	float RotationAngle = MaxDegreesPerSecond * DeltaTime * SteeringThrow;
+	FQuat RotationDelta(GetActorUpVector(), FMath::DegreesToRadians(RotationAngle));
+	Velocity = RotationDelta.RotateVector(Velocity);
+	AddActorWorldRotation(RotationDelta);
+}
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Translation = Velocity * 100 * DeltaTime;
+
+	FHitResult Hit;
+
+	AddActorWorldOffset(Translation, true, &Hit);
+
+	if (Hit.IsValidBlockingHit())
+	{
+		Velocity = FVector(0.f, 0.f, 0.f);
+	}
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AGoKart::MoveRight);
+}
+
+void AGoKart::MoveForward(float Axis)
+{
+	Throttle = Axis;
+}
+
+void AGoKart::MoveRight(float Axis)
+{
+	SteeringThrow = Axis;
+}
+
+
---
+++ Source/CrazyKarts/GoKart.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class CRAZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+
+	// The force applied to the car when the throttle is fully down (N).
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	// The number of degrees rotated per second at full control throw (degrees/s).	
+	UPROPERTY(EditAnywhere)
+	float MaxDegreesPerSecond = 90.f;
+
+	FVector Velocity;
+	float Throttle;
+	float SteeringThrow;
+
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
+	void UpdateLocationFromVelocity(float DeltaTime);
+	void UpdateCarRotation(float DeltaTime);
+};
